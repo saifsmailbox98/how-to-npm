c55b9eb24d49804b3230cf57083bd10466979991
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-01 14:48:16 +0000

    MY FIRST ASYNC I/O! (Exercise 4 of 13)

--- program.js
+++ program.js
@@ -1,4 +1,8 @@
     var fs = require('fs');
-    var str = fs.readFileSync(process.argv[2]).toString();
-    str = str.split('\n');
-    console.log(str.length -1) ;
+fs.readFile(process.argv[2],'utf8',function(err,data){
+    
+    if(!err)
+    {
+        console.log(data.split("\n").length - 1);
+    }
+})
