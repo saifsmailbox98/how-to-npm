08f63ce99ef4d5a58c8f852896f649bcf920cc77
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-01 19:53:05 +0000

    HTTP COLLECT (Exercise 8 of 13)

--- program.js
+++ program.js
@@ -1,13 +1,20 @@
 var http = require('http');
 
 http.get(process.argv[2],function(response){
+    var sum=0;
+    var string="";
     
      response.on("data", function (data) { 
          
          data=data.toString();
          
-         console.log(data);
+         sum+=data.length;
+         string+=data;
      });
-    
+     
+    response.on("end", function(){
+        console.log(sum);
+        console.log(string);
+    });
     
 });
