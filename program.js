7f642b7e9765bbbfd09c9b64c3548935b1087aa8
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-01 19:59:55 +0000

    JUGGLING ASYNC (Exercise 9 of 13)

--- program.js
+++ program.js
@@ -1,20 +1,51 @@
 var http = require('http');
 
 http.get(process.argv[2],function(response){
-    var sum=0;
+    
     var string="";
     
      response.on("data", function (data) { 
          
          data=data.toString();
-         
-         sum+=data.length;
          string+=data;
      });
      
     response.on("end", function(){
-        console.log(sum);
+   
         console.log(string);
+        http.get(process.argv[3],function(response){
+    
+    var string="";
+    
+     response.on("data", function (data) { 
+         
+         data=data.toString();
+         string+=data;
+     });
+     
+    response.on("end", function(){
+   
+        console.log(string);
+        http.get(process.argv[4],function(response){
+    
+    var string="";
+    
+     response.on("data", function (data) { 
+         
+         data=data.toString();
+         string+=data;
+     });
+     
+    response.on("end", function(){
+   
+        console.log(string);
+        
+    });
+    
+});
+    });
+    
+});
     });
     
 });
