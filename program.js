4cbe8d5d062269f458bd4a276b4330e95622e4ca
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-02 18:37:12 +0000

    HELLO WORLD! Exercise 1 of 8

--- program.js
+++ program.js
@@ -1,37 +1,10 @@
-     var http = require('http');
-     var fs = require('fs');
-     var url = require('url');
-     
-     var server = http.createServer(function (request, response) { 
-   
-          response.writeHead(200, { 'Content-Type': 'application/json' });
-          
-          
-          
-          if(url.parse(request.url,true).pathname=="/api/parsetime"){
-               
-               var date =  new Date(url.parse(request.url,true).query.iso);
-               var obj={
-                    "hour" : date.getHours(),
-                    "minute" : date.getMinutes(),  
-                    "second" : date.getSeconds()
-               }
-           response.end(JSON.stringify(obj));
-          }
-          else if(url.parse(request.url,true).pathname=="/api/unixtime"){
-               var date =  new Date(url.parse(request.url,true).query.iso);
-               var obj={
-                    "unixtime" : date.getTime()
-               }
-           response.end(JSON.stringify(obj));      
-        
-          }
-          else
-          response.end();
-          
-     }); 
-     
-     
+    var express = require('express');
     
-     
-     server.listen(process.argv[2]);  
+    var app = express();
+    
+    app.get('/home', function(request, response) {
+      response.end('Hello World!');
+    })
+    
+    
+    app.listen(process.argv[2]);
