dc01720ee37b990c2218531051e3423a28b460a0
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-02 18:27:07 +0000

    HTTP JSON API SERVER (Exercise 13 of 13)

--- program.js
+++ program.js
@@ -1,14 +1,37 @@
      var http = require('http');
      var fs = require('fs');
+     var url = require('url');
      
      var server = http.createServer(function (request, response) { 
    
-          request.on('data', function (data) {
-                    data=data.toString();
-               response.write(data.toUpperCase(),'utf8');
-            
-          });
-
+          response.writeHead(200, { 'Content-Type': 'application/json' });
+          
+          
+          
+          if(url.parse(request.url,true).pathname=="/api/parsetime"){
+               
+               var date =  new Date(url.parse(request.url,true).query.iso);
+               var obj={
+                    "hour" : date.getHours(),
+                    "minute" : date.getMinutes(),  
+                    "second" : date.getSeconds()
+               }
+           response.end(JSON.stringify(obj));
+          }
+          else if(url.parse(request.url,true).pathname=="/api/unixtime"){
+               var date =  new Date(url.parse(request.url,true).query.iso);
+               var obj={
+                    "unixtime" : date.getTime()
+               }
+           response.end(JSON.stringify(obj));      
+        
+          }
+          else
+          response.end();
+          
      }); 
+     
+     
+    
      
      server.listen(process.argv[2]);  
