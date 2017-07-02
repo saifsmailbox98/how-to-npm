8fb237b1d7758bde0560a60a0e51aa9f0d7a572c
Saif Ur Rahman <saifsmailbox98@yahoo.in>
2017-07-02 10:35:53 +0000

    HTTP FILE SERVER (Exercise 11 of 13)

--- program.js
+++ program.js
@@ -1,19 +1,9 @@
-var net = require('net');
-
-var server = net.createServer(function(socket){
-    var date = new Date();
-    var yyyy = date.getFullYear();
-    var mm = date.getMonth()+1;
-    mm = mm>9?mm:"0"+mm;
-    var dd = date.getDate()>9?date.getDate():"0"+date.getDate();
-    var hh = date.getHours();
-    hh = hh>9?hh:"0"+hh;
-    var m = date.getMinutes();
-    m = m>9?m:"0"+m;
-    var str = yyyy+"-"+mm+"-"+dd+" "+hh+":"+m;
-    //console.log(str);
-    socket.end(str+"\n");
-
-});
-
-server.listen(process.argv[2]);
+     var http = require('http');
+     var fs = require('fs');
+     
+     var server = http.createServer(function (request, response) { 
+          fs.createReadStream(process.argv[3],'utf8').pipe(response);
+     })  
+     
+     
+     server.listen(process.argv[2]);  
